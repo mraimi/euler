c33592373c785498733f8ac7b47323184372219b
Michael Raimi <mar2260@columbia.edu>
2014-02-13 14:15:19 -0500

    working

--- highlyDivisibleTriangularNumber/highlyDivisibleTriangularNumber/main.cpp
+++ highlyDivisibleTriangularNumber/highlyDivisibleTriangularNumber/main.cpp
@@ -7,12 +7,49 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+
+
+long getFactorCount(long n){
+    long factCount=0;
+    bool square=false;
+    
+   
+    
+    
+    for (int i=1; i<=(int)sqrt(n); i++) {
+        if (n%i==0) {
+            factCount++;
+        }
+        if(i*i==n){
+            square=true;
+        }
+    }
+    if(square){
+        return factCount*2-1;
+    }
+    return factCount*2;
+}
+
+
+void findHDTN(int factorMin){
+    long base=1;
+    long count = 1;
+    
+    
+    while (getFactorCount(base)<500) {
+        ++count;
+        base+=(count);
+        
+    }
+    std::cout << base << std::endl;
+}
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    findHDTN(500);
+        return 0;
 }
 
