96c090d15f061737da70fa06759e4b741992c804
Michael Raimi <mar2260@columbia.edu>
2014-02-13 12:50:14 -0500

    added highDivisibleTriangularNumber

---
+++ highlyDivisibleTriangularNumber/highlyDivisibleTriangularNumber/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  highlyDivisibleTriangularNumber
+//
+//  Created by Michael Raimi on 2/13/14.
+//  Copyright (c) 2014 Michael Raimi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
